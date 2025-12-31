8bb8e41e5a732fc0cfe4c38935bc0059ea1c400b
pawan <pawan.desamsetti@gmail.com>
2025-12-30 23:59:25 -0600

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express'); // here we are calling express library to ge
 const app = express(); // we using express to use in our app
 
 app.get('/', (req,res) =>{ //arrow functon which takes two values
-    res.send({ hi: 'there' });
+    res.send({ bye:'buddy' });
 }); // route handler asociated to given route
 
 const PORT = process.env.PORT || 5000; // dynamically figure out what port to be used (env environment variables)
